3cd6312f04d4e15c29aea6ed3816d9dc02f64525
samuel <samuel.gomes.k276@gmail.com>
2024-12-03 06:17:56 +0000

    1st Day

---
+++ day_1/day_1.py
@@ -0,0 +1,33 @@
+def problem_1():
+   l1 = []
+   l2 = []
+   difs = 0
+   with open('day_1/input_1.txt', 'r') as f:
+      for line in f:
+         num = line.split()
+         l1.append(num[0])
+         l2.append(num[1])
+
+      l1.sort()
+      l2.sort()
+
+      for i in range(len(l1)):
+         difs += abs(int(l1[i]) - int(l2[i]))
+   return difs
+
+def problem_2():
+   l1 = []
+   l2 = []
+   difs = 0
+   with open('day_1/input_1.txt', 'r') as f:
+      for line in f:
+         num = line.split()
+         l1.append(num[0])
+         l2.append(num[1])
+
+      l1.sort()
+      l2.sort()
+
+      for c in range(len(l1)):
+         difs += l2.count(l1[c]) * int(l1[c])
+   return difs
