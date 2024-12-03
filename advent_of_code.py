77b62a080aa02ae4575100871b059554d4d8d743
samuel <samuel.gomes.k276@gmail.com>
2024-12-03 06:18:11 +0000

    all days

--- advent_of_code.py
+++ advent_of_code.py
@@ -1,10 +1,10 @@
-from day_1 import sol_1
-from day_2 import sol_2
-from day_3 import sol_3
+from day_1 import day_1
+from day_2 import day_2
+from day_3 import day_3
 
 
-for i in range(1, 3):
+for i in range(1, 4):
    print(f"Day {i}")
-   print(f"Problem 1: {globals()[f'sol_{i}'].problem_1()}")
-   print(f"Problem 2: {globals()[f'sol_{i}'].problem_2()}")
+   print(f"Problem 1: {globals()[f'day_{i}'].problem_1()}")
+   print(f"Problem 2: {globals()[f'day_{i}'].problem_2()}")
    print("---------------------")
