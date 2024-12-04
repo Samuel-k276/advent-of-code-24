87a3e661b61ad929d0dad0dee2da06ca17f06d56
samuel <samuel.gomes.k276@gmail.com>
2024-12-04 06:04:30 +0000

    final version

--- advent_of_code.py
+++ advent_of_code.py
@@ -1,11 +1,22 @@
-from day_1 import day_1
-from day_2 import day_2
-from day_3 import day_3
-from day_4 import day_4
+import os
+from importlib import import_module
 
+for i in range(1, 26):
+   try:
+      globals()[f'day_{i}'] = import_module(f'day_{i}.day_{i}')
+   except ImportError as e:
+      print(f"Couldn't import day_{i}: {e}")
 
-for i in range(1, 4):
+def count_subdirectories(path='.'):
+   return len([d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))])
+
+for i in range(1, count_subdirectories()):
+   if os.path.getsize(f"day_{i}/input_{i}.txt") == 0:
+      break
+   
    print(f"Day {i}")
    print(f"Problem 1: {globals()[f'day_{i}'].problem_1()}")
    print(f"Problem 2: {globals()[f'day_{i}'].problem_2()}")
    print("---------------------")
+
+print("Made by: @Samuel-k276")
